30c2d6ea69650ade05d91c73d16f81c5a3df41e5
Ehsan Abaszadeh Darban <ehsan_ead@yahoo.com>
2024-05-21 11:22:46 +0330

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GoldWareTechApp';
+export class AppComponent implements OnInit, OnDestroy {
+  private hubConnection: signalR.HubConnection;
+  public responseCode: string | null = null;
+
+  constructor() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(`${environment.backendUrl}/messageHub`, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+      })
+      .build();
+  }
+
+  ngOnInit(): void {
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err));
+
+    this.hubConnection.on('ReceiveMessage', (message: string) => {
+      this.responseCode = message;
+    });
+  }
+
+  sendMessage(message: string): void {
+    if (message.length <= 20) {
+      this.hubConnection
+        .invoke('SendMessage', message)
+        .then(() => console.log('Message sent successfully'))
+        .catch(err => console.error('Error sending message: ', err));
+    } else {
+      console.error('Message length exceeds 20 characters');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.hubConnection.stop().catch(err => console.error(err));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { PostsComponent } from './post/posts.component';
+
+const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '******.emqxsl.com',
+  port: 8883,
+  path: '',
+  username: 'emqxtest',
+  password: '**********'
+};
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignupFormComponent    
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
